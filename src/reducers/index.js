6f6b60841c72069bebf3ff721b8c83c830f8764e
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-05-06 10:38:39 +0800

    Added initial implementation of SelectionReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
-export default combineReducers({
-  libraries: LibraryReducer
-});
+const state = {
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
+};
+
+export default combineReducers(state);
 
 // console.log(store.getState());
 // { Libraries: [{"id": 0, "title": "Webpack",, "description": "..."}, ...]}
