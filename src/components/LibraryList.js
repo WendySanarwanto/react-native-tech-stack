ae0e0b543fd49582c9a90133d42a7523a4138aca
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-05-06 08:02:06 +0800

    Created LibraryList component   and connected it to the Store object.

---
+++ src/components/LibraryList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {  View, Text, } from 'react-native';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default connect()(LibraryList);
